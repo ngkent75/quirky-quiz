b5188ce0b7d7aa174fa8787fb1807ce9bc1fe8b1
Haleigh Spurlock <haleighspurlock@hotmail.com>
2021-04-19 12:44:02 -0400

    added componenets and pages, started homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Profile from './pages/Profile';
+import Quiz from './pages/Quiz';
 import './App.css';
+import { Navbar } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div>
+      <Navbar />
+      <Header />
+      <Container>
+          <Route exact path='/' component={Homepage}/>
+          <Route exact path='/home' component={Homepage}/>
+          <Route exact path='/profile' component={Profile}/>
+          <Route exact path='/quiz' component={Quiz}/>
+      </Container>
+
+      <Footer />
     </div>
+  </Router>
   );
 }
 
