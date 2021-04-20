cd20e3e1ef8ffe93ee077d78d3f7939a1f273137
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-04-20 13:58:58 -0400

    removed fontawesome link from public index

--- client/src/components/AboutInfo/index.js
+++ client/src/components/AboutInfo/index.js
@@ -6,7 +6,7 @@ function About () {
     
     const info = [
         "Quirky Quiz is a collection of quizzes made by our user community, for our user community. Hit the arrow to learn what a Quirky Quiz is!",
-        "Quirky Quizzes are a type of personality quiz that contain a varying number of results. Each result depends on the the subject of the quiz.",
+        "Quirky Quizzes are a type of funny quiz that contain a varying number of results. Each result depends on the the subject of the quiz.",
         "As you answer each question, you reveal a little bit about yourself. After completion of the quiz, you are provided with the result that is most in line with your personality.",
         "As a user, you may not only take Quirky Quizzes, you can make them too! The Quirk level is in your hands.",
         "Still not sure about Quirky Quizzes? Try a sample quiz to catch the Quirk!"
