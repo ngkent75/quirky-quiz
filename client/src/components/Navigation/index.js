31d2bdb3c5e6f2ee04cd80609bb30f2779e7c779
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-04-20 10:31:20 -0400

    corrected Navigation links and added about page

--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './style.css';
 import { Navbar, Container, Nav } from 'react-bootstrap';
 
@@ -6,19 +7,9 @@ function Navigation() {
     return (
         <Container>
             <Navbar justify variant="tabs" defaultActiveKey="/home">
-            <Nav.Item>
-                    <Nav.Link href="/home">Active</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link eventKey="link-1">Loooonger NavLink</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                 <Nav.Link eventKey="link-2">Link</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                <Nav.Link eventKey="disabled" disabled>Disabled
-                </Nav.Link>
-                </Nav.Item>
+                <Link to="/Homepage" />
+                <Link to="/Profile" />
+                <Link to="/About" />
             </Navbar>
         </Container>
     );
