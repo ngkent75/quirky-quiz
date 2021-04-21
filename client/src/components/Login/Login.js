00018157c9e2805ac9644ce79e19fd3cc879f2ab
Uriel Rosario <urielrosario1010@gmail.com>
2021-04-21 13:08:41 -0400

    login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import "./style.css";
 
 async function loginUser(credentials) {
   return fetch("http://localhost:8080/login", {
@@ -14,44 +15,58 @@ async function loginUser(credentials) {
 export default function Login({ setToken }) {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const token = await loginUser({
       username,
       password,
+      email,
     });
     setToken(token);
   };
   return (
-    <div class="wrapper">
-      <form class="form-signin" onSubmit={handleSubmit}>
-        <h2 class="form-signin-heading">Please login</h2>
-        <input
-          type="text"
-          class="form-control"
-          name="username"
-          placeholder="Username"
-          required=""
-          onChange={(event) => setUserName(event.target.value)}
-        />
-        <input
-          type="password"
-          class="form-control"
-          name="password"
-          placeholder="Password"
-          required=""
-          onChange={(event) => setPassword(event.target.value)}
-        />
-        <button class="btn btn-lg btn-primary btn-block" type="submit">
-          Login
-        </button>
-      </form>
+    <div className="container justify-content-center w-50 align-self-center ">
+      <div className="wrapper ">
+        <form className="form-signin" onSubmit={handleSubmit}>
+          <h2 className="form-signin-heading">Please Login</h2>
+          <input
+            type="text"
+            className="form-control "
+            name="username"
+            placeholder="Username"
+            required=""
+            onChange={(event) => setUserName(event.target.value)}
+          />
+          <input
+            type="email"
+            className="form-control"
+            name="email"
+            placeholder="Email"
+            required=""
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            placeholder="Password"
+            required=""
+            onChange={(event) => setPassword(event.target.value)}
+          />
+
+          <button className="btn btn-lg btn-primary btn-block" type="submit">
+            Login
+          </button>
+          <p className="text-right">
+            Dont have an account?<a href="/Signup">Sign Up</a>
+          </p>
+        </form>
+      </div>
     </div>
   );
 }
 Login.propTypes = {
   setToken: PropTypes.func.isRequired,
 };
-
-
--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,44 +1,56 @@
 import React, { Component } from "react";
 
 class Signup extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            email: "",
-            password: "",
-            
-        }
-    }
-    handleSubmit =  async event => {
-        this.setState({
-            [event.target.name]
-        })
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      usename: "",
+      email: "",
+      password: "",
+    };
+  }
+  // handleSubmit =  async event => {
+  //     this.setState({
+  //         [event.target.name]
+  //     })
+  // }
   render() {
     return (
-      <form>
-        <h3>Sign Up</h3>
+      <>
+        <form>
+          <h3>Sign Up</h3>
 
-        <div className="form-group">
-          <label>Username</label>
-          <input type="" text className="form-control" placeholder="Username" />
-        </div>
-        <div className="form-group">
-          <label>Email</label>
-          <input type="" text className="form-control" placeholder="Email" />
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input type="" text className="form-control" placeholder="Password" />
-        </div>
+          <div className="form-group">
+            <label>Username</label>
+            <input
+              type=""
+              text
+              className="form-control"
+              placeholder="Username"
+            />
+          </div>
+          <div className="form-group">
+            <label>Email</label>
+            <input type="" text className="form-control" placeholder="Email" />
+          </div>
+          <div className="form-group">
+            <label>Password</label>
+            <input
+              type=""
+              text
+              className="form-control"
+              placeholder="Password"
+            />
+          </div>
 
-        <button type="submit" className="btn btn-primary btn-block">
-          Sign Up
-        </button>
-        <p className="text-right">
-          Already Registered <a href="#">Sign In?</a>
-        </p>
-      </form>
+          <button type="submit" className="btn btn-primary btn-block">
+            Sign Up
+          </button>
+          <p className="text-right">
+            Already Registered <a href="#">Sign In?</a>
+          </p>
+        </form>
+      </>
     );
   }
 }
