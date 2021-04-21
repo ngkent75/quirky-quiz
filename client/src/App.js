a3303b81e06d2ecf4e7978661f4f76b7f5a68a23
Uriel Rosario <urielrosario1010@gmail.com>
2021-04-21 16:44:38 -0400

    login and signup

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,29 @@
-
-import React from 'react';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
-import Homepage from './pages/Homepage';
-import Profile from './pages/Profile';
-import Quiz from './pages/Quiz';
-import About from './pages/About';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Profile from "./pages/Profile";
+import Quiz from "./pages/Quiz";
+import About from "./pages/About";
 // import Footer from './components/Footer';
-import './App.css';
-import Container from 'react-bootstrap/Container';
+import "./App.css";
+import Container from "react-bootstrap/Container";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div>
-    <Router>
-      <Container>
-        <Route exact path='/' component={Homepage}/>
-        <Route exact path='/home' component={Homepage}/>
-        <Route exact path='/profile' component={Profile}/>
-        <Route exact path='/about' component={About}/>
-        <Route exact path='/quiz' component={Quiz}/>
-       </Container>
-    </Router>
-    
+      <Router>
+        <Container>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/home" component={Homepage} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/quiz" component={Quiz} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={SignUpPage} />
+        </Container>
+      </Router>
     </div>
   );
 }
---
+++ client/src/pages/LoginPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Login from "../components/Login/Login";
+
+const LoginPage = () => {
+  return (
+    <>
+      <Navigation />
+      <Login />
+    </>
+  );
+};
+
+export default LoginPage;
