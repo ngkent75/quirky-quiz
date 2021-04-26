d58155db6236e02ddf944eb3babb2edd4fe1badd
ian mcbrayer <mcbrayer.ian@gmail.com>
2021-04-26 14:10:31 -0400

    removed results list

--- client/src/pages/UserResultPage.js
+++ client/src/pages/UserResultPage.js
@@ -6,49 +6,46 @@ import API from '../utils/API';
 
 
 const UserResultPage = () => {
-    let { id } = useParams(); 
-    const [userName, setUserName] = useState('')
-    const [results, setResults] = useState([]);
+  let { id } = useParams();
+  const [userName, setUserName] = useState('')
+  const [results, setResults] = useState([]);
 
-    useEffect(() => {
-        API.getResults(id)
-        .then(results => {
-            console.log(results)
-            setUserName(results.data.username)
-            setResults(results.data.user_results)
-        })
-    }, [id]);
+  useEffect(() => {
+    API.getResults(id)
+      .then(results => {
+        console.log(results)
+        setUserName(results.data.username)
+        setResults(results.data.user_results)
+      })
+  }, [id]);
 
-    return (
-        <>
-            <Bubbles/>
-            <Navigation />
-            <div className='past-results'>
-            <h1 className="results-page-header">{userName}'s Results</h1>
-            <table>
-              <thead>
-              <tr>
+  return (
+    <>
+      <Bubbles />
+      <Navigation />
+      <div className='past-results'>
+        <h1 className="results-page-header">{userName}'s Results</h1>
+        <table>
+          <thead>
+            <tr>
               <th>Quiz title</th>
               <th>Your Result</th>
-              </tr>
-              </thead>
-              <tbody>
-              {results.map((result) => {
-                return (
+            </tr>
+          </thead>
+          <tbody>
+            {results.map((result) => {
+              return (
                 <tr key={result.id} className="result-item">
                   <td>{result.quiz_title}</td>
                   <td>{result.result}</td>
                 </tr>
-                )
-              })}
-              </tbody>
-            </table>
-            {/* <ul className="results-list">
-            {results.map(result => <li key={result.id} classname="result-item">{result.quiz_title}{result.result}</li>)}
-            </ul>  */}
-            </div>
-        </>
-    )
+              )
+            })}
+          </tbody>
+        </table>
+      </div>
+    </>
+  )
 }
 
 export default UserResultPage;
