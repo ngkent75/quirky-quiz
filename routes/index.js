0a957f63eea274098ef382d79e951cf1a3db7836
Nathan Kent <77992445+ngkent75@users.noreply.github.com>
2021-04-26 14:19:01 -0400

    Delete index.js
    api was capitalized

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const apiRoutes = require('./API');
-// const homeRoutes = require('./homeRoutes');
-
-// router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-
-module.exports = router;
