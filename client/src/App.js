f5bd2d42dc41367bc2f63930231c2d6d0e3c965b
Uriel Rosario <urielrosario1010@gmail.com>
2021-04-26 14:13:09 -0400

    readme

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,10 @@ import "./App.css";
 import Container from "react-bootstrap/Container";
 import LoginPage from "./pages/LoginPage";
 import CreateSignUpForm from "./components/CreateSignUpForm";
-import CategoryPage from "./pages/CategoryPage"
+import CategoryPage from "./pages/CategoryPage";
 import { StoreProvider } from "./utils/GlobalState";
-import { useAuth0 } from "@auth0/auth0-react";
-import LoginBtn from "./components/LoginBtn";
 
 function App() {
-  const { isLoading } = useAuth0();
-  if (isLoading) return <div>Loading...</div>;
   return (
     <div>
       <Router>
