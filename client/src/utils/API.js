0fddb73e8cc22de86a1e07100ab9014672bdafed
Ian McBrayer <mcbrayer.ian@gmail.com>
2021-04-26 22:14:36 -0400

    Revert "added save result functionality"

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -6,11 +6,6 @@ const API = {
   getCategory: (id) => axios.get('/api/category/' + id ),
   getUser: (id) => axios.get('/api/user/' + id ),
   getResults: (id) => axios.get('/api/user/' + id ),
-  getUserId: () => axios.get('/sessionid'),
-
-  saveResult: function(resultData) {
-    return axios.post('/api/userresult', resultData)
-  },
 
   saveQuiz: function(quizData) {
     return axios.post('/api/quiz', quizData);
